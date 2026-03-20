ce296f8b9d78e94e2fddf69ab42a6999c3c4ab92
naveen5005 <bellamnaveen314@gmail.com>
2026-03-20 11:23:20 +0530

    using useReducer

--- date-counter-challenge/src/App.js
+++ date-counter-challenge/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import DateCounter from './components/DateCounter';
 import DateCounterV2 from './components/DateCounterV2';
+import DateCounterReducer from './components/DateCounterReducer';
 
 function App() {
   return (
     <div className="App">
       {/* <DateCounter/> */}
-      <DateCounterV2/>
+      {/* <DateCounterV2/> */}
+      <DateCounterReducer/>
     </div>
   );
 }
